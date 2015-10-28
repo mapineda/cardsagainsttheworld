2cac97b8507e7239a4bfdeb2590afc172f10f31c
mapineda Pineda <mapineda1222@gmail.com>
2015-10-28 17:24:42 -0500

    making some changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Card = require('../models/card');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/cards', function(req, res, next) {
+	Card.find({ }, function(err, cards) {
+	  if (err) console.log(err);
+	  
+	  res.json(cards);
+	});
 });
 
 module.exports = router;
