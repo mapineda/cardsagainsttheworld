42bffaec8129dce66facb80d24bfe92455a5459e
mapineda Pineda <mapineda1222@gmail.com>
2015-10-28 14:44:29 -0500

    why is this not working

--- public/javascripts/controllers/cardsController.js
+++ public/javascripts/controllers/cardsController.js
@@ -2,32 +2,15 @@
 	var app = angular.module('CardsAgainstAssembly');
 
 	app.controller('cardsController', function() {
-		
+
 		this.cards = [
-			{question: 'I get by with a little help from ______', show: false }, 
-			{question: 'The field trip was ruined by _______', show: false },
-			{question: 'Backbone is a ___________', show: false },
-			{question: 'This guy __________(s)', show: false}
+			{question: 'I get by with a little help from ______' }, 
+			{question: 'The field trip was ruined by _______' },
+			{question: 'Backbone is a ___________'},
+			{question: 'This guy __________(s)'}
 		];
 
 
 		return this;
 	});
 })();
-
-// (function() {
-// 	var app = angular.module('CardsAgainstAssembly');
-
-// 	app.controller('cardsController', function() {
-
-// 		this.cards = [
-// 			{ question: 'I couldn\'t complete my assignment because ________', show: false },
-// 			{ question: 'I get by with a little help from ________', show: false },
-// 			{ question: 'The field trip was completely ruined by ________', show: false },
-// 			{ question: 'Kyle is a ________', show: false },
-// 			{ question: 'What is my secret power?', show: false }
-// 		];
-
-// 		return this;
-// 	});
-// })();
