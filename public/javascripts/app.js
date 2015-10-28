fcd2aec456acca1f2578c6cca9343525679eb3f1
mapineda Pineda <mapineda1222@gmail.com>
2015-10-28 15:59:51 -0500

    added new rule route

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,19 @@
 (function() {
-	var app = angular.module('CardsAgainstAssembly', []);
+	var app = angular.module('CardsAgainstAssembly', ['ui.router']);
+
+	app.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+	    .state('home', {
+	      url: '/',
+	      templateUrl: '_homeView.html',
+	    }).state('new', {
+	      url: '/new',
+	      templateUrl: '_newView.html',
+	    }).state('rules', {
+	    	url: '/rules',
+	    	templateUrl: '_rulesView.html',
+	    });
+	});
 })();
